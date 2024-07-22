3689e9a51d18b981107ad9fdabf1760d33dc17b0
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-22 12:39:06 +0530

    Update Reverse a String.java

--- Reverse a String.java
+++ Reverse a String.java
@@ -1,1 +1,8 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed string: " + reversed);
+    }
+}
 
