434866b5ba496564046900f71bad3d6d768e753e
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-22 12:51:33 +0530

    Create Integer to String .java

---
+++ Integer to String .java
@@ -0,0 +1,7 @@
+public class IntToString {
+    public static void main(String[] args) {
+        int num = 123;
+        String str = Integer.toString(num);
+        System.out.println("String: " + str);
+    }
+}
